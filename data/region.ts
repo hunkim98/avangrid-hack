25deb37c1a5d98836a2bce23f53802027bae51b9
Hun Kim <hunkim98@gmail.com>
2024-11-16 15:45:03 -0500

    Deploy buttons

---
+++ data/region.ts
@@ -0,0 +1,1 @@
+export const RTO_REGION = [[], [], []]
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Resizer from '@/components/graph/d3/Resizer'
+import { Flex, Select, Title } from '@mantine/core'
 import UsMap from '@/components/graph/d3/UsMap'
-import { Flex, Title } from '@mantine/core'
+import { Batteries } from '@/data/battery'
 import Layout from '@/components/Layout'
 import { Filter } from '@/types/filter'
 import { RTO_NAME } from '@/data/rto'
@@ -10,15 +11,28 @@ interface IndexPageProps {}
 
 const IndexPage: React.FC<IndexPageProps> = () => {
   const [filter, setFilter] = useState<Filter>({ state: null, rtoIndex: null })
+
   return (
     <Layout>
-      <Flex flex={1} h={400}>
-        <Flex direction={'column'} flex={1} h={400} maw={500}>
-          <Resizer>
-            <UsMap setFilter={setFilter} filter={filter} width={800} height={600} />
-          </Resizer>
+      <Flex direction={'column'} justify={'center'} gap={10}>
+        <Flex flex={1} h={400}>
+          <Flex direction={'column'} flex={1} h={400} maw={500}>
+            <Resizer>
+              <UsMap setFilter={setFilter} filter={filter} width={800} height={600} />
+            </Resizer>
+          </Flex>
+          {filter.rtoIndex !== null && <Title>{RTO_NAME[filter.rtoIndex]}</Title>}
         </Flex>
-        {filter.rtoIndex !== null && <Title>{RTO_NAME[filter.rtoIndex]}</Title>}
+        <Select
+          label={'Industry Location'}
+          data={Batteries.map((val) => val.name)}
+          value={'None'}
+        />
+        <Select
+          label={'Battery Storage Choice'}
+          data={Batteries.map((val) => val.name)}
+          value={Batteries[0].name}
+        />
       </Flex>
     </Layout>
   )
